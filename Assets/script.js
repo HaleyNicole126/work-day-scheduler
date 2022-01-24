7b3129b97f6f0dd5e83e6934d6c4ccbb48c74afb
haybales <haleynicole126@gmail.com>
2022-01-23 18:44:19 -0700

    time blocks

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,13 @@
 document.getElementById("currentDay").innerHTML = moment().format("dddd, MMMM D");
 
+document.getElementById("nine").innerHTML = moment().hour(9).minute(0).format("h:mm a");
+document.getElementById("ten").innerHTML = moment().hour(10).minute(0).format("h:mm a");
+document.getElementById("eleven").innerHTML = moment().hour(11).minute(0).format("h:mm a");
+document.getElementById("noon").innerHTML = moment().hour(12).minute(0).format("h:mm a");
+document.getElementById("one").innerHTML = moment().hour(13).minute(0).format("h:mm a");
+document.getElementById("two").innerHTML = moment().hour(14).minute(0).format("h:mm a");
+document.getElementById("three").innerHTML = moment().hour(15).minute(0).format("h:mm a");
+document.getElementById("four").innerHTML = moment().hour(16).minute(0).format("h:mm a");
+document.getElementById("five").innerHTML = moment().hour(17).minute(0).format("h:mm a");
+
 
